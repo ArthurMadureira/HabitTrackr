b0e7b8467650c721f5665b2433a567117be1803c
Arthur Madureira <arthurmadureira10@gmail.com>
2023-01-18 18:33:38 -0300

    day range generation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import '../src/styles/global.css'
-import { Habit } from "./components/Habit";
 import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
 
 export function App() {
@@ -8,6 +8,8 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+
+        <SummaryTable />
       </div>
     </div>
   )
