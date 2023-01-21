4c4ad7fd06d0f93e332e684673a9e7a7baecd4b1
Arthur Madureira <arthurmadureira10@gmail.com>
2023-01-20 21:06:37 -0300

    mobile: habits of the day interface

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,6 @@
-import { View, TouchableOpacity, Dimensions } from "react-native";
+import { View, TouchableOpacity, TouchableOpacityProps, Dimensions } from "react-native";
+
+interface Props extends TouchableOpacityProps { }
 
 const WEEK_DAYS = 7
 const SCREEN_HORINZONTAL_PADDING = (32 * 2) / 5
@@ -6,12 +8,13 @@ const SCREEN_HORINZONTAL_PADDING = (32 * 2) / 5
 export const DAY_MARGIN_BETWEEN = 8
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORINZONTAL_PADDING + 5)
 
-export function HabitDay() {
+export function HabitDay({ ...rest }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-900"
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      {...rest}
     />
   )
 }
